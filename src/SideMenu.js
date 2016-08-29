d2c260ce5989daaedf34733a8ea7393a0875cbaf
rokfortuna <rokfortunaslo@gmail.com>
2016-08-29 15:11:12 +0200

    Default theme styling added, icons, chevrons etc.

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -4,7 +4,8 @@ export default class SideMenu extends Component {
 
   propTypes : {
     items: PropTypes.array,
-    onMenuItemClick: PropTypes.func
+    onMenuItemClick: PropTypes.func,
+    theme: PropTypes.string
   }
 
   constructor(props) {
@@ -27,26 +28,8 @@ export default class SideMenu extends Component {
   }
 
   componentDidMount() {
-    console.log('DID MOUNT');
     const {items} = this.props;
-    console.log(this.buildTree(items, null));
     this.setState({itemTree: this.buildTree(items, null)});
-    console.log(this.state);
-    // items.forEach((item) => this.setInactiveState(item));
-  }
-
-
-  activateItem(item) {
-    const {itemTree} = this.state;
-    const self = this;
-    return (e) => {
-      item.active = true;
-      console.log('ACTIVATE ITEM');
-      console.log(item);
-      console.log(items);
-      console.log(e);
-      this.setState({items: items});
-    }
   }
 
   deactivateTree(itemTree) {
@@ -71,45 +54,84 @@ export default class SideMenu extends Component {
     const self = this;
     return (e) => {
       e.stopPropagation();
-      item.active = true;
-      self.deactivateTree(itemTree);
-      self.activeParentPath(item);
-      self.setState({itemTree: itemTree});
-      console.log(item);
+      e.nativeEvent.stopImmediatePropagation();
+      if (!item.active) {
+        item.active = true;
+        self.deactivateTree(itemTree);
+        self.activeParentPath(item);
+        self.setState({itemTree: itemTree});
+      } else {
+        item.active = false;
+        self.deactivateTree(itemTree);
+        if (item.parent) {
+          self.activeParentPath(item.parent);
+        }
+        self.setState({itemTree: itemTree});
+      }
     }
   }
 
+  renderChevron (item) {
+    if (item.children && item.children.length > 0) {
+      if (item.active) {
+        return (<i className="fa fa-chevron-down"></i>);
+      } else {
+        return (<i className="fa fa-chevron-left"></i>);
+      }
+    }
+    return null;
+  }
+
   renderItem(item, level) {
-    const {onMenuItemClick} = this.props;
-    return (
-      <div key={item.value}
-      onClick={this.onItemClick(item)}>
-        {item.children.length > 0 &&
-          <span> {item.label} </span>
-        }
-        {item.children.length === 0 && !onMenuItemClick &&
-          <a href={`#${item.value}`}>
+    const {onMenuItemClick, theme} = this.props;
+
+    if (item.divider) {
+      return (<div key={item.value} className={`divider divider-level-${level}`}>{item.label} </div>);
+    }
+    else {
+      return (<div
+        key={item.value}
+        onClick={this.onItemClick(item)}
+        className={`item item-level-${level} ${item.active ? 'active': ''}`}
+        >
+        <div className="item-title">
+          {/* render icon if provided*/}
+          {item.icon &&
+            <i className={`fa ${item.icon} item-icon`}> </i>
+          }
+          {/* render a simple label if not a leaf element */}
+          {item.children.length > 0 &&
             <span> {item.label} </span>
-          </a>
-        }
-        {item.children.length === 0 && onMenuItemClick &&
-          <span onClick={()=> onMenuItemClick(item.value)}> {item.label} </span>
-        }
-        <div className={`children ${item.active ? 'active' : 'inactive'}`} style={{paddingLeft: `${level * 20}px`}}>
+          }
+          {/* render a link if it is a leaf and no onMenuItemClick is provided */}
+          {item.children.length === 0 && !onMenuItemClick &&
+            <a href={`#${item.value}`}>
+              <span> {item.label} </span>
+            </a>
+          }
+          {/* use onMenuItemClick if provided */}
+          {item.children.length === 0 && onMenuItemClick &&
+            <span onClick={()=> onMenuItemClick(item.value)}> {item.label} </span>
+          }
+          {/* render fa chevrons for default theme */}
+          { (!theme || theme == 'default') && this.renderChevron(item)}
+        </div>
+        {/* render children */}
+        <div className={`children ${item.active ? 'active' : 'inactive'}`}>
           {item.children && item.children.map((child) =>
             this.renderItem(child, level + 1)
           )}
         </div>
-      </div>
-    );
+      </div>);
+    }
   }
 
   render() {
-    console.log(this.state);
     const {itemTree} = this.state;
+    const {theme} = this.props;
 
     return (
-      <div className="Side-menu">
+      <div className={`Side-menu ${theme ? `Side-menu-${theme}` : 'Side-menu-default'} children active`}>
         {itemTree && itemTree.map((item) =>
           this.renderItem(item, 1)
         )}
